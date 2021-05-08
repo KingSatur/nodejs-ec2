06ec6a97ae17e4d1126e9e399ede9832f1fdb7a8
Juan David Campillo Cepeda <juancawool@outlook.com>
2021-05-08 00:37:13 -0500

    Changing port

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
     "<h1>Express with html</h1>"
   );
 });
-app.listen(3000);
-console.log("Server on port 3000");
+app.listen(8080);
+console.log("Server on port 8080");
