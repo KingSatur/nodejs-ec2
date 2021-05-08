c2c481a5a8fea7cfc48ca115f2315693661e7115
Juan David Campillo Cepeda <juancawool@outlook.com>
2021-05-08 00:33:03 -0500

    Html response

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hellow world express");
+  res.send(<h1>Express with html</h1>);
 });
 app.listen(3000);
 console.log("Server on port 3000");
