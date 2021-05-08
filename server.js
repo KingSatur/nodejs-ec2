1c6953063523a4a37adb2950c7b39ecd3065650f
Juan David Campillo Cepeda <juancawool@outlook.com>
2021-05-08 00:34:26 -0500

    Sample

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(<h1>Express with html</h1>);
+  res.send(
+    "<h1>Express with html</h1>"
+  );
 });
 app.listen(3000);
 console.log("Server on port 3000");
